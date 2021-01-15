01416376a9a2c797157699c43bf80d1c8ce84e4d
martentyrk <martentyrk@gmail.com>
2021-01-15 12:33:40 +0200

    Vue template update

--- ChatApp/chatapp/src/App.vue
+++ ChatApp/chatapp/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChatWindow/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatWindow from './components/ChatWindow.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatWindow
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: #3e66bd;
+  }
+  #app {    
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ ChatApp/chatapp/src/components/ChatWindow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="chat-window">
+    <textarea name="chatbox" id="chatbox" cols="40" rows="5" placeholder="Type here..."></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .chat-window {
+        background-color: white;
+        margin: 150px auto;
+        width: 350px;
+        height: 500px;
+        border-width: 0.5;
+        border-style: solid;
+        border-color: gray;
+        position: relative
+    }
+    #chatbox {
+      border-color: orange;
+      resize: none;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 344px;
+    }
+</style>
